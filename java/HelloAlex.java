359515fa7b58f60005445d47ff97d7f706364776
alex <alex@sparky.alexbride.org>
2018-02-25 20:46:30 -0700

    alex first commit :-)

---
+++ java/HelloAlex.java
@@ -0,0 +1,7 @@
+public class HelloAlex {
+
+    public static void main(String args[]) {
+        System.out.println("Hello Alex");
+    }
+
+}
---
+++ java/HelloMommy.java
@@ -0,0 +1,7 @@
+public class HelloMommy {
+
+    public static void main(String args[]) {
+        System.out.println("Hello daddy. everything is awesome");
+    }
+
+}
